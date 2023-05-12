cb709f0304fd9fd41b5cd2a89e50b264d90e566b
김선진 <tjswls8062@skuniv.ac.kr>
2023-05-12 17:47:27 +0900

    feat: navbar & login page

--- enjoytrip-vue/src/router/index.js
+++ enjoytrip-vue/src/router/index.js
@@ -1,24 +1,55 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    // path: '/',
-    // name: 'home',
-    // component: HomeView
+    path: '/',
+    name: "HomeView",
+    component: HomeView
   },
   {
-    // path: '/about',
-    // name: 'about',
-    // // route level code-splitting
-    // // this generates a separate chunk (about.[hash].js) for this route
-    // // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/user",
+    name: "UserView",
+    component: () => import(/* webpackChunkName: "user" */ "../views/UserView.vue"),
+    children: [
+      {
+        path: "login",
+        name: "loginView",
+        component: () => import(/* webpackChunkName: "user" */ "../components/user/UserLogin.vue"),
+      },
+      {
+        path: "join",
+        name: "joinView",
+        component: () =>
+          import(/* webpackChunkName: "user" */ "../components/user/UserJoin.vue"),
+      },
+      {
+        path: "findId",
+        name: "findIdView",
+        component: () =>
+          import(/* webpackChunkName: "user" */ "../components/user/UserFindId.vue"),
+      },
+      {
+        path: "findPw",
+        name: "findPwView",
+        component: () =>
+          import(/* webpackChunkName: "user" */ "../components/user/UserFindPw.vue"),
+      },
+    ],
+  },
+  
+  // {
+  //   // path: '/about',
+  //   // name: 'about',
+  //   // // route level code-splitting
+  //   // // this generates a separate chunk (about.[hash].js) for this route
+  //   // // which is lazy-loaded when the route is visited.
+  //   // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
+];
 
 const router = new VueRouter({
   mode: 'history',
@@ -26,4 +57,4 @@ const router = new VueRouter({
   routes
 })
 
-export default router
+export default router;
