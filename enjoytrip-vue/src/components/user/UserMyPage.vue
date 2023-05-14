a7a25bebdf18f93226bb4a7fd762241c1fe00186
김선진 <tjswls8062@skuniv.ac.kr>
2023-05-14 16:16:07 +0900

    edit: notice xml, serviceImpl

--- enjoytrip-vue/src/components/user/UserMyPage.vue
+++ enjoytrip-vue/src/components/user/UserMyPage.vue
@@ -9,18 +9,18 @@
     </div>
 
       <!-- 회원 정보 수정이 눌려있다면 -->
-    <!-- <div class="titleEdit">
+    <div class="titleEdit">
       회원 정보 수정
     </div> -->
-    <!-- <user-edit></user-edit> -->
+    <user-edit></user-edit>
     
     
       <!-- 좋아요가 눌려있다면 -->
-    <div class="title">
+    <!-- <div class="title">
       좋아요한 목록
     </div>
     
-    <user-like-list></user-like-list>
+    <user-like-list></user-like-list> -->
     
 
     </div>
@@ -29,15 +29,15 @@
 </template>
 
 <script>
-// import UserEdit from './item/UserEdit.vue';
-import UserLikeList from './item/UserLikeList.vue';
+import UserEdit from './item/UserEdit.vue';
+// import UserLikeList from './item/UserLikeList.vue';
 import HeaderNaviBar from "../layout/HeaderNaviBar.vue";
 
 export default {
   name: "UserMyPage",
   components: {
-    // UserEdit,
-    UserLikeList,
+    UserEdit,
+    // UserLikeList,
     HeaderNaviBar,    
   }
 }
