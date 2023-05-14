895d879144f0b2de3915363cf69ce9a36bde07de
김선진 <tjswls8062@skuniv.ac.kr>
2023-05-14 16:45:09 +0900

    edit: rm..

--- enjoytrip-springboot/src/main/java/com/ssafy/enjoytrip/attraction/model/AttractionDescriptionDTO.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.enjoytrip.attraction.model;
-
-public class AttractionDescriptionDTO {
-	private int content_id;
-	private String homepage;
-	private String overview;
-	private String telname;
-}
