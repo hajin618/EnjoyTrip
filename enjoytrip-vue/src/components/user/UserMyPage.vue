371f6b9c3744b734c11d01f0d60da6b8db2f6b38
김선진 <tjswls8062@skuniv.ac.kr>
2023-05-16 17:42:52 +0900

    feat: mypage edit

--- enjoytrip-vue/src/components/user/UserMyPage.vue
+++ enjoytrip-vue/src/components/user/UserMyPage.vue
@@ -3,41 +3,40 @@
     <header-navi-bar></header-navi-bar>
     <div class="main">
       <!-- 회원 정보 수정이 눌려있다면 초기값은 좋아요임 -->
-    <div class="tabBar">
-      <button class="tabBarBtn">회원 정보 수정</button>
-      <button class="tabBarBtn">좋아요 목록 보기</button>
-    </div>
+      <div class="tabBar">
+        <button class="tabBarBtn">회원 정보 수정</button>
+        <button class="tabBarBtn">좋아요 목록 보기</button>
+      </div>
 
       <!-- 회원 정보 수정이 눌려있다면 -->
-    <div class="titleEdit">
+    <!-- <div class="titleEdit">
       회원 정보 수정
-    </div> -->
-    <user-edit></user-edit>
+    </div>
+    <user-edit></user-edit> -->
     
     
       <!-- 좋아요가 눌려있다면 -->
-    <!-- <div class="title">
-      좋아요한 목록
-    </div>
-    
-    <user-like-list></user-like-list> -->
-    
-
+      <div class="title">
+        좋아요한 목록
+      </div>
+      <user-review-list></user-review-list>
     </div>
     
   </div>
 </template>
 
 <script>
-import UserEdit from './item/UserEdit.vue';
-// import UserLikeList from './item/UserLikeList.vue';
+// import UserEdit from './item/UserEdit.vue';
+// import UserPlanList from './item/UserPlanList.vue';
+import UserReviewList from './item/UserReviewList.vue';
 import HeaderNaviBar from "../layout/HeaderNaviBar.vue";
 
 export default {
   name: "UserMyPage",
   components: {
-    UserEdit,
-    // UserLikeList,
+    // UserEdit,
+    // UserPlanList,
+    UserReviewList,
     HeaderNaviBar,    
   }
 }
